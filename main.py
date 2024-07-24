ef09e52e7d6ce6f603957fb8d6e019ba1b31e5eb
kevinchang <35583291@qq.com>
2024-07-24 17:55:40 +0800

    refactor(main): switch server list format from txt to json
    Change the file format for server lists from plain text to JSON, which
    provides better structure and ease of parsing for server data. Update
    the ServerListFile opening logic and print_server_info function to
    align with the new JSON format.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import pexpect
 import json
-from termcolor import colored, cprint
+from defines import *
 
 r"""
 
@@ -46,7 +46,7 @@ debug = Config["Debug"]
 if debug:
     cprint("Debug Enabled", "yellow", attrs=["bold"])
 # 打开服务器列表文件
-ServerListFile = open("Servers.txt", "r")
+ServerListFile = open("Servers.json", "r")
 # 读取文件内容
 ServerListFileData = ServerListFile.read()
 # 调试模式下打印文件内容
@@ -57,43 +57,6 @@ ServerList = json.loads(ServerListFileData)
 # 调试模式下打印解析后的数据
 if debug:
     print("Serverlist(JsonDecode):", ServerList)
-
-
-def print_server_info(info, server_data, server_num):
-    """
-    打印服务器信息。
-
-    :param info: 需要打印的信息类型（名称、IP、端口、用户名、密码或所有信息）。
-    :param server_data: 服务器数据列表。
-    :param server_num: 服务器编号。
-    """
-    if info == "name":
-        print(colored("ServerName:", "yellow"), end="")
-        cprint(server_data[server_num]["name"], "green")
-    elif info == "ip":
-        print(colored("ServerIP:", "yellow"), end="")
-        cprint(server_data[server_num]["ip"], "green")
-    elif info == "port":
-        print(colored("ServerPort:", "yellow"), end="")
-        cprint(server_data[server_num]["port"], "green")
-    elif info == "username":
-        print(colored("ServerUsername:", "yellow"), end="")
-        cprint(server_data[server_num]["username"], "green")
-    elif info == "password":
-        print(colored("ServerPassword:", "yellow"), end="")
-        cprint(server_data[server_num]["password"], "green")
-    elif info == "all":
-        print(colored("ServerName:", "yellow"), end="")
-        cprint(server_data[server_num]["name"], "green")
-        print(colored("ServerIP:", "yellow"), end="")
-        cprint(server_data[server_num]["ip"], "green")
-        print(colored("ServerPort:", "yellow"), end="")
-        cprint(server_data[server_num]["port"], "green")
-        print(colored("ServerUsername:", "yellow"), end="")
-        cprint(server_data[server_num]["username"], "green")
-        print(colored("ServerPassword:", "yellow"), end="")
-        cprint(server_data[server_num]["password"], "green")
-
 
 # 调试模式下打印第一台服务器的名称
 if debug:
